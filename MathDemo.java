123f4da00b05cc2cadb360e00ae9d1606187700e
csushma1 <suco6938@colorado.edu>
2017-05-31 10:41:54 -0600

    Add files via upload

---
+++ MathDemo.java
@@ -0,0 +1,19 @@
+
+
+import java.lang.*;
+
+public class MathDemo {
+
+   public static void main(String[] args) {
+
+      // get two double numbers numbers
+      double x = 15;
+      double y = 25;
+
+      // print the square root of these doubles
+      System.out.println("Math.sqrt(" + x + ")=" + (Math.sqrt(x)));
+      System.out.println("Math.sqrt(" + y + ")=" + Math.sqrt(y));
+   }
+}
+//compile javac MathDemo.java
+//run java MathDemo
